1fc9398bb09a32c6a079771ed288562db2ae813b
Riley Lodge <riley.lodge@gmail.com>
2025-06-20 15:49:57 +0930

    added gui

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -47,43 +47,62 @@ function capitalize(word){
 
 
 
+function playRound(humanChoice, computerChoice){
 
-
-function playGame(){
-
-    let humanScore = 0
-    let computerScore = 0
-
-    function playRound(humanChoice, computerChoice){
-
+    humanChoice = humanChoice.toLowerCase()
     let win = 0
 
     if (humanChoice == computerChoice){
         console.log("It's a draw, you both picked "+ capitalize(humanChoice))
-        return
+        return ("It's a draw, you both picked "+ capitalize(humanChoice))
     }
     else if ((humanChoice == "rock" && computerChoice == "paper") 
         || (humanChoice == "paper" && computerChoice == "scissors") 
         || (humanChoice == "scissors" && computerChoice == "rock") ){
             console.log("You lose, loser, " + capitalize(computerChoice) + " beats " + capitalize(humanChoice))
             computerScore+=1;
-            return
+            return "You lose, loser, " + capitalize(computerChoice) + " beats " + capitalize(humanChoice)
 
     }
     else {
         console.log(`You win, ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`)
         humanScore+=1;
-        return
+        return `You win, ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`
     }
 
+}
+
+buttons = document.querySelectorAll("button")
+
+
+function button_callback(btn){
+    const result = playRound(btn.textContent, getComputerChoice())
+
+    result_statement = document.createElement('p');
+    result_statement.textContent = result 
+    result_statement.setAttribute("id","statement")
+    const score = "\n Score: " + humanScore + ' to '+ computerScore;
+    const score_statement = document.createElement('p')
+    score_statement.setAttribute("id","statement")
+    score_statement.textContent=score;
+
+    if (humanScore>=5){
+        score_statement.textContent = "YOU WIN THE GAME!"
+        score_statement.style.fontWeight="bold"
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if(computerScore>=5){
+        score_statement.textContent = "YOU LOSE THE GAME!"
+        score_statement.style.fontWeight="bold"
+        humanScore = 0;
+        computerScore = 0;
     }
 
-    for (i=0; i<5; i++){
-        playRound(getHumanChoice(),getComputerChoice())
-    }
-
-    console.log(`Score = ${humanScore} to ${computerScore}`)
+    document.querySelector("body").appendChild(result_statement);
+    document.querySelector("body").appendChild(score_statement);
 }
 
 
-playGame()
+buttons.forEach( button =>  button.addEventListener("click", e => button_callback(button)) );
+
